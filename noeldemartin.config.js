26ef21cd5cf3e3858d9ed538df77004f1bf3017c
Noel De Martin <noeldemartin@gmail.com>
2021-09-26 13:00:45 +0200

    Update config format

--- noeldemartin.config.js
+++ noeldemartin.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const { defineConfig } = require('@noeldemartin/scripts');
+
+module.exports = defineConfig({
     name: 'NoelDeMartinSolidUtils',
     declarations: [
         // 'src/plugins/cypress/types.d.ts',
         'src/plugins/jest/types.d.ts',
     ],
-};
+});
