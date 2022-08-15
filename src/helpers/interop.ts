50cf83f3260cb09d7b2bcb58480a6696f8596d1c
Noel De Martin <noel@noeldemartin.com>
2022-08-15 12:42:01 +0200

    Refactor type index methods

--- src/helpers/interop.ts
+++ src/helpers/interop.ts
@@ -1,37 +1,40 @@
 import { uuid } from '@noeldemartin/utils';
 
 import { createSolidDocument, fetchSolidDocument, solidDocumentExists, updateSolidDocument } from '@/helpers/io';
-import type SolidThing from '@/models/SolidThing';
 import type { Fetch } from '@/helpers/io';
 import type { SolidUserProfile } from '@/helpers/auth';
 
-async function mintPrivateTypeIndexUrl(user: SolidUserProfile, fetch?: Fetch): Promise<string> {
+type TypeIndexType = 'public' | 'private';
+
+async function mintTypeIndexUrl(user: SolidUserProfile, type: TypeIndexType, fetch?: Fetch): Promise<string> {
     fetch = fetch ?? window.fetch.bind(fetch);
 
     const storageUrl = user.storageUrls[0];
-    const typeIndexUrl = `${storageUrl}settings/privateTypeIndex`;
+    const typeIndexUrl = `${storageUrl}settings/${type}TypeIndex`;
 
     return await solidDocumentExists(typeIndexUrl, fetch)
-        ? `${storageUrl}settings/privateTypeIndex-${uuid()}`
+        ? `${storageUrl}settings/${type}TypeIndex-${uuid()}`
         : typeIndexUrl;
 }
 
-export async function createPrivateTypeIndex(user: SolidUserProfile, fetch?: Fetch): Promise<string> {
+async function createTypeIndex(user: SolidUserProfile, type: TypeIndexType, fetch?: Fetch) {
     if (user.writableProfileUrl === null) {
         throw new Error('Can\'t create type index without a writable profile document');
     }
 
     fetch = fetch ?? window.fetch.bind(fetch);
 
-    const typeIndexUrl = await mintPrivateTypeIndexUrl(user, fetch);
-    const typeIndexBody = `
-        <> a
-            <http://www.w3.org/ns/solid/terms#TypeIndex>,
-            <http://www.w3.org/ns/solid/terms#UnlistedDocument> .
-    `;
+    const typeIndexUrl = await mintTypeIndexUrl(user, type, fetch);
+    const typeIndexBody = type === 'public'
+        ? '<> a <http://www.w3.org/ns/solid/terms#TypeIndex> .'
+        : `
+            <> a
+                <http://www.w3.org/ns/solid/terms#TypeIndex>,
+                <http://www.w3.org/ns/solid/terms#UnlistedDocument> .
+        `;
     const profileUpdateBody = `
         INSERT DATA {
-            <${user.webId}> <http://www.w3.org/ns/solid/terms#privateTypeIndex> <${typeIndexUrl}> .
+            <${user.webId}> <http://www.w3.org/ns/solid/terms#${type}TypeIndex> <${typeIndexUrl}> .
         }
     `;
 
@@ -40,24 +43,53 @@ export async function createPrivateTypeIndex(user: SolidUserProfile, fetch?: Fet
         updateSolidDocument(user.writableProfileUrl, profileUpdateBody, fetch),
     ]);
 
+    if (type === 'public') {
+        // TODO Implement updating ACLs for the listing itself to public
+    }
+
     return typeIndexUrl;
 }
 
-export async function findContainerRegistration(
+async function findRegistrations(
     typeIndexUrl: string,
-    childrenType: string,
+    type: string | string[],
+    predicate: string,
     fetch?: Fetch,
-): Promise<SolidThing | null> {
+): Promise<string[]> {
     const typeIndex = await fetchSolidDocument(typeIndexUrl, fetch);
-    const containerQuad = typeIndex
-        .statements(undefined, 'rdf:type', 'solid:TypeRegistration')
-        .find(
-            statement =>
-                typeIndex.contains(statement.subject.value, 'solid:forClass', childrenType) &&
-                typeIndex.contains(statement.subject.value, 'solid:instanceContainer'),
-        );
+    const types = Array.isArray(type) ? type : [type];
 
-    return containerQuad
-        ? typeIndex.getThing(containerQuad.subject.value) ?? null
-        : null;
+    return types.map(
+        type => typeIndex
+            .statements(undefined, 'rdf:type', 'solid:TypeRegistration')
+            .filter(statement => typeIndex.contains(statement.subject.value, 'solid:forClass', type))
+            .map(statement => typeIndex.statements(statement.subject.value, predicate))
+            .flat()
+            .map(statement => statement.object.value)
+            .filter(url => !!url),
+    ).flat();
+}
+
+export async function createPublicTypeIndex(user: SolidUserProfile, fetch?: Fetch): Promise<string> {
+    return createTypeIndex(user, 'public', fetch);
+}
+
+export async function createPrivateTypeIndex(user: SolidUserProfile, fetch?: Fetch): Promise<string> {
+    return createTypeIndex(user, 'private', fetch);
+}
+
+export async function findContainerRegistrations(
+    typeIndexUrl: string,
+    type: string | string[],
+    fetch?: Fetch,
+): Promise<string[]> {
+    return findRegistrations(typeIndexUrl, type, 'solid:instanceContainer', fetch);
+}
+
+export async function findInstanceRegistrations(
+    typeIndexUrl: string,
+    type: string | string[],
+    fetch?: Fetch,
+): Promise<string[]> {
+    return findRegistrations(typeIndexUrl, type, 'solid:instance', fetch);
 }
