627358750e005fbc04f56073c262ec7b89affec0
Noel De Martin <noel@noeldemartin.com>
2022-11-20 10:52:33 +0100

    Implement compacting JSON-LD graphs

--- src/helpers/jsonld.ts
+++ src/helpers/jsonld.ts
@@ -1,3 +1,6 @@
+import { compact } from 'jsonld';
+import type { JsonLdDocument } from 'jsonld';
+
 export type JsonLD = Partial<{
     '@context': Record<string, unknown>;
     '@id': string;
@@ -6,6 +9,20 @@ export type JsonLD = Partial<{
 
 export type JsonLDResource = Omit<JsonLD, '@id'> & { '@id': string };
 export type JsonLDGraph = { '@graph': JsonLDResource[] };
+
+export async function compactJsonLDGraph(jsonld: JsonLDGraph): Promise<JsonLDGraph> {
+    const compactedJsonLD = await compact(jsonld as JsonLdDocument, {});
+
+    if ('@graph' in compactedJsonLD) {
+        return compactedJsonLD as JsonLDGraph;
+    }
+
+    if ('@id' in compactedJsonLD) {
+        return { '@graph': [compactedJsonLD] } as JsonLDGraph;
+    }
+
+    return { '@graph': [] };
+}
 
 export function isJsonLDGraph(jsonld: JsonLD): jsonld is JsonLDGraph {
     return '@graph' in jsonld;
