70fba2db4562da63a3ae210c80936d148acfc0de
Noel De Martin <noel@noeldemartin.com>
2025-03-24 12:10:45 +0100

    Verify npm package and fix lint warnings

--- src/testing/vitest/index.ts
+++ src/testing/vitest/index.ts
@@ -4,7 +4,9 @@ import matchers from './matchers';
 
 export type VitestSolidMatchers<R = unknown> = {
     [K in keyof typeof matchers]: (
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
         ...args: Parameters<(typeof matchers)[K]> extends [any, ...infer Rest] ? Rest : never
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
     ) => ReturnType<(typeof matchers)[K]> extends Promise<any> ? Promise<R> : R;
 };
 
