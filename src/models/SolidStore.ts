b037ae51509043f95baac21b45cb15cbaf2876f9
Noel De Martin <noel@noeldemartin.com>
2026-01-18 11:46:52 +0100

    Improve SolidStore types

--- src/models/SolidStore.ts
+++ src/models/SolidStore.ts
@@ -1,4 +1,13 @@
-import type { BlankNode, Literal, NamedNode, Quad, Variable } from '@rdfjs/types';
+import type {
+    BlankNode,
+    Literal,
+    NamedNode,
+    Quad,
+    Quad_Object,
+    Quad_Predicate,
+    Quad_Subject,
+    Variable,
+} from '@rdfjs/types';
 
 import { expandIRI } from '@noeldemartin/solid-utils/helpers/vocabs';
 
@@ -26,7 +35,11 @@ export default class SolidStore {
         this.quads.push(...quads);
     }
 
-    public statements(subject?: Term | string, predicate?: Term | string, object?: Term | string): Quad[] {
+    public statements(
+        subject?: Quad_Subject | string,
+        predicate?: Quad_Predicate | string,
+        object?: Quad_Object | string,
+    ): Quad[] {
         return this.quads.filter(
             (statement) =>
                 (!object || this.termMatches(statement.object, object)) &&
@@ -35,7 +48,11 @@ export default class SolidStore {
         );
     }
 
-    public statement(subject?: Term | string, predicate?: Term | string, object?: Term | string): Quad | null {
+    public statement(
+        subject?: Quad_Subject | string,
+        predicate?: Quad_Predicate | string,
+        object?: Quad_Object | string,
+    ): Quad | null {
         const statement = this.quads.find(
             (_statement) =>
                 (!object || this.termMatches(_statement.object, object)) &&
@@ -46,26 +63,26 @@ export default class SolidStore {
         return statement ?? null;
     }
 
-    public contains(subject: string, predicate?: string, object?: string): boolean {
+    public contains(
+        subject: Quad_Subject | string,
+        predicate?: Quad_Predicate | string,
+        object?: Quad_Object | string,
+    ): boolean {
         return this.statement(subject, predicate, object) !== null;
     }
 
-    public getThing(subject: string): SolidThing {
+    public getThing(subject: Quad_Subject | string): SolidThing {
         const statements = this.statements(subject);
 
-        return new SolidThing(subject, statements);
-    }
-
-    protected expandIRI(iri: string): string {
-        return expandIRI(iri);
+        return new SolidThing(typeof subject === 'string' ? subject : subject.value, statements);
     }
 
     protected termMatches(term: Term, value: Term | string): boolean {
         if (typeof value === 'string') {
-            return this.expandIRI(value) === term.value;
+            return expandIRI(value) === term.value;
         }
 
-        return term.termType === term.termType && term.value === value.value;
+        return term.equals(value);
     }
 
 }
