7ea9e28b004ba86775be8b5ba5120f59729635c2
Noel De Martin <noel@noeldemartin.com>
2026-01-26 06:40:43 +0100

    Implement deprecated error aliases for backwards compatibility

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -4,3 +4,33 @@ export { default as NotFound } from './NotFound';
 export { default as Unauthorized } from './Unauthorized';
 export { default as UnsuccessfulNetworkRequest } from './UnsuccessfulNetworkRequest';
 export { default as UnsupportedAuthorizationProtocol } from './UnsupportedAuthorizationProtocol';
+
+/**
+ * @deprecated Use MalformedSolidDocument instead.
+ */
+export { default as MalformedSolidDocumentError } from './MalformedSolidDocument';
+
+/**
+ * @deprecated Use NetworkRequestFailed instead.
+ */
+export { default as NetworkRequestError } from './NetworkRequestFailed';
+
+/**
+ * @deprecated Use NotFound instead.
+ */
+export { default as NotFoundError } from './NotFound';
+
+/**
+ * @deprecated Use Unauthorized instead.
+ */
+export { default as UnauthorizedError } from './Unauthorized';
+
+/**
+ * @deprecated Use UnsuccessfulNetworkRequest instead.
+ */
+export { default as UnsuccessfulNetworkRequestError } from './UnsuccessfulNetworkRequest';
+
+/**
+ * @deprecated Use UnsupportedAuthorizationProtocol instead.
+ */
+export { default as UnsupportedAuthorizationProtocolError } from './UnsupportedAuthorizationProtocol';
